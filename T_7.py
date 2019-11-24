81397b3812aebea9133490249c1c6e279b5fc4fe
Ramoo25 <Ramoo25@mail.ru>
2019-11-24 09:46:30 +0300

    T_7_faster

--- T_7.py
+++ T_7.py
@@ -1,19 +1,63 @@
 import time
-
+#104743
 start_time = time.time()
-x = 2
-i = 3
 
-while (x <= 10001):
-  z = 2
-  while (z < int(i**0.5)+1):
-    if (i % z == 0):
-      break
-    z += 1
-  else:
-    x += 1
-  i += 1
+mas = []
 
+num_max = 10001  #STOP CONDITION
+n_mas = 30000      #count in one cicle
+start_mas = 2  #const
 
-print("answer: ",i-1)
+u = []
+
+for i in range(0,n_mas):
+  mas.append(start_mas+i)
+for i in range(len(mas)):
+  if mas[i]==0:
+    continue
+  u.append([mas[i],0])
+  j = i
+  for j in range(i+mas[i],len(mas),mas[i]):
+    mas[j] = 0
+  u[len(u)-1][1]=j+2
+
+#print("mas",mas)
+start_mas += n_mas
+kol = 0
+flag = False
+for i in range(len(mas)):
+  if mas[i] != 0:
+    kol += 1
+  if kol == num_max:
+    print("answer: ", mas[i])
+    flag = True
+    break
+n = 1
+while not flag:
+  for i in range(0,n_mas):
+    mas[i] = start_mas+i
+  for i in range(len(u)):
+    step = u[i][0]
+    poz = u[i][1]
+    poz += step
+    #print(step,poz)
+    #print(poz-n*n_mas-2)
+    while poz < start_mas+n_mas:
+      mas[poz-n*n_mas-2] = 0
+      poz += step
+    u[i][1] = poz-step
+  flag = False
+  for i in range(len(mas)):
+    if (mas[i]!=0):
+      kol += 1
+      u.append([mas[i],mas[i]])
+      if kol == num_max:
+        print("answer: ",mas[i])
+        flag = True
+        break        
+  n+=1
+  start_mas += n_mas
+  #print ("mas",mas)
+
+#print (mas)
 print(time.time()-start_time)
